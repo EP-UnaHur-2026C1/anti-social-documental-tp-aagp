89effe5adbb2e8fe0be58cb2f17c5b440e33ea9d
Gonzalo <58990971+gonnbar@users.noreply.github.com>
2026-06-19 10:13:55 -0300

    Update validarImage.js

--- src/middlewares/validarImage.js
+++ src/middlewares/validarImage.js
@@ -1,6 +1,6 @@
 const imgSchema = require('../schemas/image.schema')
 const validarImage = (req,res,next) =>{
-    const { error } = tagSchema.validate(req.body)
+    const { error } = imgSchema.validate(req.body)
     if (error) {
         return res.status(400).json({error: error.details[0].message})
     }

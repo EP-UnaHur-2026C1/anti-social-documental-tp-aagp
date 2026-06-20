6e7c5fbf072568fcda95f0b77d3ea5ea49919263
pazm-avila <pazmaria.avila@estudiantes.unahur.edu.ar>
2026-06-20 13:03:22 -0300

    fix controller actualizar y eliminar post con redis

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,9 +6,9 @@ const validarIdUser = require('../middlewares/validarUserId');
 const validarPostCache = require('../middlewares/validarPostCache');
 
 //tags imports
-const validarTagId = require('../middlewares/validarTagId');
-const validarTagsArray = require('../middlewares/validarTagsPost');
-const validarUpdatePost = require('../middlewares/validarPostAct');
+//const validarTagId = require('../middlewares/validarTagId');
+//const validarTagsArray = require('../middlewares/validarTagsPost');
+//const validarUpdatePost = require('../middlewares/validarPostAct');
 
 // VALIDAR TAG EXISTE ---> ARRAY DE TAGS NO SIRVE PARA AGREGAR UNO SOLO
 const validarTagExiste = require("../middlewares/validarExistenciaTags")
@@ -31,13 +31,13 @@ const {
 router.get("/", validarPostCache, obtenerPosts);
 router.get("/:id", validarIdPost, obtenerPostPorId);
 router.post("/", validarIdUser, validarPost, publicarPost);
-router.patch("/:id", validarIdPost, validarPost, actualizarPost);
+router.patch("/:id", validarIdPost, validarPost, actualizarPost); // validarUpdatePost
 router.delete("/:id", validarIdPost, eliminarPost);
 
 // TAG
-router.patch("/:id/tags/:tagId", validarIdPost, validarTagId, agregarTagAPost)
-router.patch("/:id/tags", validarIdPost, validarTagsArray, agregarTagsAPost)
-router.delete("/:id/tags/:tagId", validarIdPost, validarTagId, quitarTagAPost)
-router.delete("/:id/tags", validarIdPost, validarTagsArray, quitarTodosLosTagsAPost)
+//router.patch("/:id/tags/:tagId", validarIdPost, validarTagId, agregarTagAPost)
+//router.patch("/:id/tags", validarIdPost, validarTagsArray, agregarTagsAPost)
+//router.delete("/:id/tags/:tagId", validarIdPost, validarTagId, quitarTagAPost)
+//router.delete("/:id/tags", validarIdPost, validarTagsArray, quitarTodosLosTagsAPost)
 
 module.exports = router;

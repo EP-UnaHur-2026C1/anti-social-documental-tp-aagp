392c07a9df661e274f713dde966739af81e3c0d2
pazm-avila <pazmaria.avila@estudiantes.unahur.edu.ar>
2026-06-23 11:21:17 -0300

    Agregadas funciones para asociar Tags a Post con REDIS

--- src/middlewares/validarExistenciaTags.js
+++ src/middlewares/validarExistenciaTags.js
@@ -6,6 +6,9 @@ const validarQueTagsExistan = async (req, res, next) => {
         const tagsEncontrados = await Tag.find({
             _id: { $in: tags }
         });
+        if (!req.body.tags) {
+            return next();
+        }
         if (tagsEncontrados.length !== tags.length) {
             return res.status(404).json({
                 message: "Uno o más tags ingresados no existen"
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,17 +4,13 @@ const validarIdPost = require('../middlewares/validarPostId');
 const validarPost = require('../middlewares/validarPost');
 const validarIdUser = require('../middlewares/validarUserId');
 const validarPostCache = require('../middlewares/validarPostCache');
+const validarUpdatePost = require('../middlewares/validarPostAct');
 
 //tags imports
-//const validarTagId = require('../middlewares/validarTagId');
-//const validarTagsArray = require('../middlewares/validarTagsPost');
-//const validarUpdatePost = require('../middlewares/validarPostAct');
+const validarIdTag = require("../middlewares/existenciaUnicoTag"); // Valida ID específico para :tagsId
+const validarTagsArray = require('../middlewares/validarTagsPost'); // Valida input
+const validarTags = require("../middlewares/validarExistenciaTags"); // Valida existencia
 
-const validarPostParActualizar = require('../middlewares/validarPostAct')
-
-const validarTagExiste = require("../middlewares/validarExistenciaTags")
-const validarUserId = require("../middlewares/validarUserId")
-const validarUnicoTagExistente = require("../middlewares/existenciaUnicoTag")
 
 const {
     obtenerPosts,
@@ -29,32 +25,14 @@ const {
 } = require("../controllers/post.controller");
 
 router.get("/", validarPostCache, obtenerPosts);
-
 router.get("/:id", validarIdPost, obtenerPostPorId);
-router.post("/", validarIdUser, validarPost, publicarPost);
-router.patch("/:id", validarIdPost, validarPost, actualizarPost); // validarUpdatePost
+router.post("/", validarIdUser, validarPost, validarTags, publicarPost);
+router.patch("/:id", validarIdPost, validarUpdatePost, actualizarPost);
 router.delete("/:id", validarIdPost, eliminarPost);
 
-/*
-router.get("/:id", validarId, validarIdPost, obtenerPostPorId);
-router.post("/", validarPost,validarTagExiste,validarUserId,publicarPost);
-router.patch("/:id", validarId, validarIdPost, validarPostParActualizar, actualizarPost);
-router.delete("/:id", validarId, validarIdPost, eliminarPost);
-*/
-
-
-
-
 // TAG
-//router.patch("/:id/tags/:tagId", validarIdPost, validarTagId, agregarTagAPost)
-//router.patch("/:id/tags", validarIdPost, validarTagsArray, agregarTagsAPost)
-//router.delete("/:id/tags/:tagId", validarIdPost, validarTagId, quitarTagAPost)
-//router.delete("/:id/tags", validarIdPost, validarTagsArray, quitarTodosLosTagsAPost)
+router.patch("/:id/tags", validarIdPost, validarTagsArray, validarTags, agregarTagsAPost);
+router.delete("/:id/tags/:tagId", validarIdPost, validarIdTag, quitarTagAPost);
+router.delete("/:id/tags", validarIdPost, validarTagsArray, validarTags, quitarTodosLosTagsAPost);
 
-
-//develop
-router.patch("/:id/tags/:tagId", validarId,validarUnicoTagExistente,agregarTagAPost) // funciona
-router.patch("/:id/tags", validarId, agregarTagsAPost) // funciona
-router.delete("/:id/tags/:tagId", validarId, quitarTagAPost) //
-router.delete("/:id/tags", validarId, quitarTodosLosTagsAPost) // funciona
 module.exports = router;

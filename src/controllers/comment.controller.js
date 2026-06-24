8d10e3874c9f1f79c74f3991ad0349ed2c447a69
ailenperalta <ailen.peralta@estudiantes.unahur.edu.ar>
2026-06-24 19:24:22 -0300

    Ajuste en controller de comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -35,7 +35,7 @@ const obtenerComentario = async (req, res) => {
 const crearComentario = async (req, res) => {
     try {
         const comentario = await Comment.create(req.body);
-        res.status(201).json(comentario);
+        res.status(201).json({message: "Comentario creado correctamente"});
     } catch (error) {
         res.status(500).json({
             message: "Error al crear el comentario",
